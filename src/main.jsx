060d8c50076ea2e0ce68846cc795b280d284a3ac
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-31 13:24:36 -0500

    init project

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {VidlyApp} from './VidlyApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VidlyApp />
+  </React.StrictMode>
+)
