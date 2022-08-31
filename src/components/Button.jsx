e656f179fbc8378b4fad49e307d718428a0aba00
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-31 16:00:20 -0500

    apply eslint rules

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,11 @@
-export const Button = ({ onclick, children, bgcolor, textcolor, hover = '', width = '' }) => {
+export const Button = ({
+  onclick,
+  children,
+  bgcolor,
+  textcolor,
+  hover = '',
+  width = ''
+}) => {
   return (
     <button
       className={`${bgcolor} px-4 py-2 ${textcolor} py-2 px-5 rounded 
