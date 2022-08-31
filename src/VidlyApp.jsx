6cbd4a480c7ea918465e0173d15905ba923832bd
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-31 13:35:49 -0500

    add reac-router-dom dependency

--- src/VidlyApp.jsx
+++ src/VidlyApp.jsx
@@ -1,15 +1,9 @@
 export const VidlyApp = () => {
-
   return (
-    <div>
-      <h1 className="text-3xl">Hello world!</h1>
+    <div className='w-full h-screen p-5 flex flex-col items-center justify-center space-y-5'>
+      <h1 className='text-3xl'>Hello world!</h1>
 
-      <div class="grid grid-rows-3 grid-flow-col gap-4">
-  <div class="row-span-3 ...">01</div>
-  <div class="col-span-2 ...">02</div>
-  <div class="row-span-2 col-span-2 ...">03</div>
-</div>
+      <div className='w-full h-full bg-secondary rounded-lg' />
     </div>
   )
 }
-
