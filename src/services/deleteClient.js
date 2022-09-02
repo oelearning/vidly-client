7c001a7babb04974870ff669ee5d384162e5bf21
Oscar Estrada <arturostrada2020@gmail.com>
2022-09-01 20:04:50 -0500

    fix id param bug

--- src/services/deleteClient.js
+++ src/services/deleteClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const deleteClient = ({ id }) => {
+export const deleteClient = (id) => {
   return axios.delete(`http://localhost:3000/api/customers/${id}`)
     .then(response => {
       const { data } = response
